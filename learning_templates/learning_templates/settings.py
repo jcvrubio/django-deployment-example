a7e9ca1025ae4a63274f1ae16d3b11d546296389
jcvrubio <58215716+jcvrubio@users.noreply.github.com>
2020-05-26 22:38:35 +0400

    Update settings.py
    Set ALLOWED_HOSTS = ['*']

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v383xm*%#3tew+nsx38q97n3!gqqt))e!23_*oyu$mv@3hd04b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
